8d5970ee8c0164aa0e2e7a3c6fc880265c477bb8
nathaly-12 <129960987+nathaly-12@users.noreply.github.com>
2023-08-02 12:01:20 -0300

    Add files via upload

---
+++ main.sql
@@ -0,0 +1,48 @@
+CREATE TABLE comida_pais (
+    id_pais INTEGER PRIMARY KEY NOT NULL,
+    pais VARCHAR(50) NOT NULL
+);
+
+
+INSERT INTO comida_pais VALUES (
+    1,
+    'BRASIL'
+);
+
+INSERT INTO comida_pais VALUES (
+    2,
+    'MEXICO'
+);
+
+INSERT INTO comida_pais VALUES (
+    3,
+    'JAPAO'
+);
+
+INSERT INTO comida_pais VALUES (
+    4,
+    'ITALIA'
+);
+
+SELECT * FROM comida_pais;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
